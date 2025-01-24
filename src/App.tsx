6dc3ab7defc05f0fc87356b375d7c60065c9f96a
Thalles Alexsander <thallesander@hotmail.com>
2025-01-24 14:45:18 -0300

    creates the container for the tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './styles/App.css'
 import ContentProfile from './components/ContentProfile'
 import ProgressBar from './components/ProgressBar'
+import ContentInfo from './components/ContentInfo'
 
 function App() {
   return (
     <>
       <div className='full-width-div'>
+        <div className='column'>
         <ContentProfile />
         <ProgressBar/>
+        </div>
+        <div className='column'>
+        <ContentInfo/>
+        </div>
+        
       </div>
     </>
   )
--- src/components/ContentInfo.tsx
+++ src/components/ContentInfo.tsx
@@ -1,1 +1,14 @@
 import '../styles/Component.css'
+
+function ContentInfo() {
+  return (
+    <div className='content-info-div'>
+        <form action="" className='form-task'>
+            <input type="" placeholder='Type your task' className='input-task'/>
+            <button>Add Task</button>
+        </form>
+    </div>
+  )
+}
+
+export default ContentInfo
