ad8bfcdb49b3c6e98fdadae1468094555a7ceb77
Thalles Alexsander <thallesander@hotmail.com>
2025-01-24 14:24:13 -0300

    creates the first components and styles them

---
+++ src/components/ContentProfile.tsx
@@ -0,0 +1,15 @@
+import '../styles/Component.css'
+
+function ContentProfile() {
+  return (
+    <div className='component-profile-div'>
+        <button>PROFILE 1</button>
+        <button>PROFILE 2</button>
+        <p>Full space to daily quotes</p>
+    </div>
+  )
+}
+
+export default ContentProfile
+
+
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,14 @@
+import '../styles/Component.css'
+
+function ProgressBar() {
+  return (
+    <div className='progress-bar-div'>
+        <div className='progress-bar'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ProgressBar
