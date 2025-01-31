c7639381545c7452e93989271e45d8d6ad70d034
Thalles Alexsander <thallesander@hotmail.com>
2025-01-31 10:55:11 -0300

    adds a addTask function

--- src/components/ContentInfo.tsx
+++ src/components/ContentInfo.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import '../styles/Component.css'
 
 function ContentInfo() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+
+  const addTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(newTask.trim()) {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  
+
   return (
     <div className='content-info-div'>
         <form action="" className='form-task'>
